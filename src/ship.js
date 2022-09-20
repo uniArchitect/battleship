fedc725bb436a08a1eee5283eeec17c924c20e8a
uniArchitect <hancho93@gmail.com>
2022-09-19 20:11:23 -0400

    Add hit and isSunk function to Ship class.

--- src/ship.js
+++ src/ship.js
@@ -1,7 +1,20 @@
 class Ship {
     constructor(length) {
         this.length = length;
-        this.hitLocation = [];
+        this.hitSquare = false;
         this.isSunk = false;
+    }
+
+    hit = (attSquare) => {
+        if (gameSquare == attSquare) {
+            let hitSquare = 'hit';
+            return hitSquare;
+        }
+    }
+
+    isSunk = (length, hitSquare) => {
+        if (Ship.hitSquare == 'hit') {
+            return true;
+        }
     }
 }
