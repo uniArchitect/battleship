d9bf6e5093ecf2efa31cfdaac883126e515a70fb
uniArchitect <hancho93@gmail.com>
2022-09-22 23:03:54 -0400

    Edit Gameboard constructor function to create empty two dimensional array.

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -5,6 +5,40 @@ class Gameboard {
   constructor(rows, cols) {
     this.rows = rows;
     this.cols = cols;
+
+    let board = [];
+
+    // Create empty two dimensional array
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            board[i] = [];
+        }
+    }
+
+    return board;
+
+    // program to create a two dimensional array
+    // function twoDimensionArray(a, b) {
+    //     let arr = [];
+
+    //     // creating two dimensional array
+    //     for (let i = 0; i< a; i++) {
+    //         for(let j = 0; j< b; j++) {
+    //             arr[i] = [];
+    //         }
+    //     }
+
+    //     // inserting elements to array
+    //     for (let i = 0; i< a; i++) {
+    //         for(let j = 0; j< b; j++) {
+    //             arr[i][j] = j;
+    //         }
+    //     }
+    //     return arr;
+    // }
+
+    // const x = 2;
+    // const y = 3;
   }
 
   // Event - Place ships (object from Ship.js)
--- src/test/Gameboard.test.js
+++ src/test/Gameboard.test.js
@@ -1,6 +1,15 @@
 /* eslint-disable no-undef */
 const Gameboard = require("../Gameboard");
 
+test("Gameboard test empty array", () => {
+    expect(new Gameboard(3,1)).toEqual({
+        rows: 3,
+        cols: 3,
+
+
+    })
+})
+
 test("Gameboard call Ship class constructor", () => {
     expect(Gameboard.placeShip(5)).toEqual({
       length: 5,
