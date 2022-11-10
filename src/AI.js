e2e236e718d6df21f37a69250599fc2ea5c13c15
uniArchitect <hancho93@gmail.com>
2022-11-09 21:26:01 -0500

    Add computerAttackMove in Computer class to output into Gameboard functions placeShip and receiveAttack.

--- src/AI.js
+++ src/AI.js
@@ -3,6 +3,12 @@ class Computer {
         this.name = name;
     }
 
+    // x,y arguments should be randomly generated to be input into computerPlace / computerAttack OR Gameboard class functions placeShip, and receiveAttack
+    // attackMove will be output into Gameboard functions
+    static computerAttackMove = (x,y) => {
+        return [x,y];
+    }
+
     // computerPlace function
 
     // computerAttack function
