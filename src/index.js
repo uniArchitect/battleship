3c4af982ecdfb88996b194b5f490c71f180ee1bd
uniArchitect <hancho93@gmail.com>
2022-12-01 22:30:46 -0500

    Add separate variable for computerBoard in index.js to create computer gameboard.

--- src/index.js
+++ src/index.js
@@ -5,20 +5,25 @@ import Gameboard from "./Gameboard";
 import Player from "./Player";
 
 const playerContainer = document.querySelector(".Player-1-Container");
+const computerContainer = document.querySelector(".Player-2-Container");
 
 // In progress function to create battleship gameboard
 const gameBoard = new Gameboard(10, 10);
 
 const playerBoard = document.createElement("div");
 playerContainer.appendChild(playerBoard).className = "player-board";
+const computerBoard = document.createElement("div");
+computerContainer.appendChild(computerBoard).className = "computer-board";
 
-const createBoard = (rows, cols) => {
-  playerBoard.style.setProperty("--grid-rows", rows);
-  playerBoard.style.setProperty("--grid-cols", cols);
+const createBoard = (board, rows, cols) => {
+    board.style.setProperty("--grid-rows", rows);
+    board.style.setProperty("--grid-cols", cols);
   for (let i = 0; i < rows * cols; i++) {
     let playerSquare = document.createElement("div");
-    playerSquare.setAttribute("id", "player-square");
-    playerBoard.appendChild(playerSquare).className = "player-square";
+    playerSquare.setAttribute("id", "board-square");
+    board.appendChild(playerSquare).className = "board-square";
   }
 };
-createBoard(gameBoard.rows, gameBoard.cols);
+
+createBoard(playerBoard, gameBoard.rows, gameBoard.cols);
+createBoard(computerBoard, gameBoard.rows, gameBoard.cols);
