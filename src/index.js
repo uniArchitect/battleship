9dda8efff1fcd614e9128ba196f7db66ee148898
uniArchitect <hancho93@gmail.com>
2022-12-03 16:20:16 -0500

    Define changeCoordinate function to change boardSquare element to miss marker.

--- src/index.js
+++ src/index.js
@@ -16,60 +16,58 @@ const computerBoard = document.createElement("div");
 computerContainer.appendChild(computerBoard).className = "computer-board";
 
 const createBoard = (board, rows, cols) => {
-    board.style.setProperty("--grid-rows", rows);
-    board.style.setProperty("--grid-cols", cols);
-  
-    for (let i = 0; i < rows; i++) {
-        board[i] = [];
-        for (let j = 0; j < cols; j++) {
-          board[i][j] = 0;
-            let playerSquare = document.createElement("div");
-            playerSquare.setAttribute("id", `${i},${j}`);
-            board.appendChild(playerSquare).className = "board-square";   
-        }
-      }
+  board.style.setProperty("--grid-rows", rows);
+  board.style.setProperty("--grid-cols", cols);
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < cols; j++) {
+      board[i][j] = 0;
+      let playerSquare = document.createElement("div");
+      playerSquare.setAttribute("id", `${i},${j}`);
+      board.appendChild(playerSquare).className = "board-square";
+    }
+  }
 };
 
 createBoard(playerBoard, gameBoard.rows, gameBoard.cols);
 createBoard(computerBoard, gameBoard.rows, gameBoard.cols);
 
 // Render Battleship Attack on Click
-const boardSquare = document.querySelectorAll('.board-square');
+const boardSquare = document.querySelectorAll(".board-square");
 
 // Mark each board square
+const changeCoordinate = (element, marker) => {
+  element.setAttribute("id", `${marker}`);
+  element.appendChild(marker);
+};
+
 const attackCoordinate = (e) => {
-    
-    console.log(e.target);
-    
-    // Define miss marker
-    const miss = document.createElement('div');
-    miss.classList.add('miss')
-    miss.innerHTML = `
-    <svg style="width:7em;height:7em" viewBox="0 0 24 24">
+  console.log(e.target);
+
+  // Define miss marker
+  const miss = document.createElement("div");
+  miss.classList.add("miss");
+  miss.innerHTML = `
+    <svg style="width:1.75em;height:1.75em" viewBox="0 0 24 24">
         <path fill="white" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
     </svg>
     `;
 
-    // Define hit marker
-    const hit = document.createElement('div');
-    hit.classList.add('circle');
-    hit.innerHTML = `
-    <svg style="width:6em;height:6em" viewBox="0 0 24 24">
+  // Define hit marker
+  const hit = document.createElement("div");
+  hit.classList.add("circle");
+  hit.innerHTML = `
+    <svg style="width:1.75em;height:1.75em" viewBox="0 0 24 24">
         <path fill="white" d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
     </svg>
-    `; 
+    `;
 
-    changeCoordinate = (element, marker) => {
-        console.log(element);
-        element.setAttribute('id', `${marker}`);
-        element.appendChild(marker);
-    }
-
-    // Change game square
-    changeCoordinate(e.target, marker);
+  // Change game square
+  changeCoordinate(e.target, miss);
 };
 
 // Add event listener for each board square
-boardSquare.forEach(boardSquare => {
-    boardSquare.addEventListener('click', attackCoordinate, {once:true});
-})
+boardSquare.forEach((boardSquare) => {
+  boardSquare.addEventListener("click", attackCoordinate, { once: true });
+});
